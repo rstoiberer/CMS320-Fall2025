78dfd93231c4e2f731f71e43a933b0a8036f714f
Richard Stoiberer <rstoiberer@rollins.edu>
2025-11-17 09:24:06 -0500

    Bug Fixes
    Does not constantly die anymore. jump animation could not be fixed. Death animation still does not have enough time to play

--- MistyRainforest/Assets/Scripts/Player/KomeaOneShotDeath.cs
+++ MistyRainforest/Assets/Scripts/Player/KomeaOneShotDeath.cs
@@ -1,16 +1,36 @@
+using System.Collections;                     // <-- add this
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class KomeaOneShotDeath : MonoBehaviour
-
 {
     public Animator animator;
+    public float deathDelay = 1.0f;          // time to show death anim
+    private bool isDying = false;            // prevent double-kill
 
     public void KillPlayer()
     {
-        // Play death SFX/anim here if you want, then reload.
-        //animator.Play("Dead");
-        //animator.SetBool("isDead", true);
+        if (isDying) return;
+        isDying = true;
+
+        // 1) Trigger death animation
+        if (animator != null)
+        {
+            animator.SetBool("isDead", true);   // makes Animator go to Dead state
+        }
+
+        // 2) Optional: disable movement script so player can't move while dead
+        var controller = GetComponent<KomeaMovement2>();   // or whatever your script is called
+        if (controller != null) controller.enabled = false;
+
+        // 3) Start delayed reload
+        StartCoroutine(DeathRoutine());
+    }
+
+    private IEnumerator DeathRoutine()
+    {
+        yield return new WaitForSeconds(deathDelay);
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
